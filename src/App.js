9758a464079d6e7a4a0d48c076fd605caa823d38
Viet Vo <viet.allen.qvo@gmail.com>
2019-01-17 14:26:05 -0800

    morning update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
 import LandingZone from './components/LandingZone';
 import SignUp from './components/SignUp';
+
+Amplify.configure(aws_exports);
+
 
 class App extends Component {
   render() {
@@ -33,4 +40,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
